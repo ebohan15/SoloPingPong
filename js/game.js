1b525a02585289d264a871b6cc072a8f1e727164
Ethan Bohan <ebohan15@gmail.com>
2016-06-28 14:48:38 -0400

    Week 02 changes
    starting to build solo pong game

--- js/game.js
+++ js/game.js
@@ -19,4 +19,37 @@ window.cancelRequestAnimFrame = ( function() {
 		clearTimeout;
 } )();
 
-console.log('Holla');
+//DO NOT TOUCH CODE ABOVE
+
+//console.log('Holla');
+
+//step1..erb..create game canvas and track mouse position
+var gameCanvas = document.getElementById("canvas");
+// Store Html5 canvas tag in js varible 
+
+var ctx = gameCanvas.getContext("2d"); // Create context 2d
+var W = window.innerWidth;
+var H = window.innerHeight;
+var mouseObj = {};
+
+//console.log('browser width is currently ' + W)
+//console.log('browser height is currently ' + H)
+
+gameCanvas.width = W;
+gameCanvas.height = H;
+
+function paintCanvas() {
+    ctx.fillStyle = "#000000"
+    ctx.fillRect(0, 0, W, H);
+}
+paintCanvas();
+
+
+function trackPosition(evt){
+    mouseObj.x = evt.pageX;
+    mouseObj.y = evt.pageY;
+    console.log("cursor x is :" + mouseObj.x + "cursor y is :" + mouseObj.y);
+}
+
+gameCanvas.addEventListener("mousemove", trackPosition, true);
+
