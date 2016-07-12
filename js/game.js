15c8edd649ba75046b69c9757b34d1628533bc18
Ethan Bohan <ebohan15@gmail.com>
2016-07-12 12:27:01 -0400

    added stuff

--- js/game.js
+++ js/game.js
@@ -53,3 +53,72 @@ function trackPosition(evt){
 
 gameCanvas.addEventListener("mousemove", trackPosition, true);
 
+//step2..erb..clear page canvas by covering it in black
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//step3..erb..place a ball on the canvas
+var ball = {};//ball object
+ball = {
+    x: 50,
+    y: 50,
+    r: 5,
+    c: "#ffffff",
+    vx: 4,
+    vy: 8,
+    
+    draw: function(){
+        ctx.beginPath();
+        ctx.fillStyle = this.c;
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, false);
+        ctx.fill();
+    }
+}
+ball.draw();
+
+//step4..erb..place a start button on canvas
+var startBtn = {};// start button 
+startBtn = {
+    w:100,
+    h:50,
+    x: W / 2 - 50,
+    y: H / 2 - 25,
+    
+    draw: function () {
+        ctx.strokeStyle = "#ffffff";
+        ctx.lineWidth = "2";
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+    
+        ctx.font = "18px Arial, sans-seriff";
+        ctx.textAlign = "center";
+        ctx.fillBaseline = "middle";
+        ctx.fillStyle='#ffffff';
+        ctx.fillText("Start", W/2, H/2);
+    }
+}
+startBtn.draw();
+
+//step5..erb..place score and pionts on canvas
+var points = 0; //game points
+function paintScore() {
+    ctx.fillStyle = "#ffffff";
+    ctx.font = "18px Arial, sans-seriff";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("Score: " + points, 20, 20);
+}
+paintScore();
+
+
